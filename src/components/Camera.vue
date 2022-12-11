080e05b4f738e9a0db98855d1878bdf632ce8d67
quizdy <quizdynet@gmail.com>
2022-12-11 23:00:57 +0900

    first commit

---
+++ src/components/Camera.vue
@@ -0,0 +1,25 @@
+<script setup>
+const webcam = ref(null);
+onMounted(() => {
+  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices
+      .getUserMedia({ audio: false, video: true })
+      .then((stream) => {
+        webcam.srcObject = stream;
+      });
+  }
+});
+</script>
+
+<template>
+  <div>
+    <video ref="webcam" autoplay></video>
+  </div>
+</template>
+
+<style scoped>
+video {
+  height: 80vh;
+  width: 90vw;
+}
+</style>
