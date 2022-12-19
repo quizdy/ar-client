d0221de66564ed4261f3af067f28dff36ed11931
quizdy <quizdynet@gmail.com>
2022-12-19 12:18:33 +0900

    20221218

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,4 @@
-import path from "path";
-import fs from "fs";
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default {
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, "./keys/server.key")),
-      cert: fs.readFileSync(path.resolve(__dirname, "./keys/server.csr")),
-    },
-  },
   srcDir: "./src",
 };
