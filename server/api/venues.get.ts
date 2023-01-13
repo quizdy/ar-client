45518b747d95807f57a9dd59d43c54832331f62a
quizdy <quizdynet@gmail.com>
2023-01-13 12:38:17 +0900

    20230113debug3

--- server/api/venues.get.ts
+++ server/api/venues.get.ts
@@ -7,16 +7,22 @@ const JSONS_PATH = "../../assets/jsons"
 
 export default defineEventHandler(() => {
   const jsonsDir = path.join(__dirname, JSONS_PATH)
-  const jsons = fs.readdirSync(jsonsDir)
+  // const jsons = fs.readdirSync(jsonsDir)
   const venues = []
-  for(let i = 0; i < jsons.length; i++) {
-    const venue = {
-      no: i,
-      title: path.basename(jsons[i], '.json'),
-      image: "/images/" + path.basename(jsons[i], '.json') + "/_.png"
-    }
-    venues.push(venue)
+  // for(let i = 0; i < jsons.length; i++) {
+  //   const venue = {
+  //     no: i,
+  //     title: path.basename(jsons[i], '.json'),
+  //     image: "/images/" + path.basename(jsons[i], '.json') + "/_.png"
+  //   }
+  //   venues.push(venue)
+  // }
+  const venue = {
+    no: 1,
+    title: 'aa.json',
+    image: "/images/aa/_.png"
   }
+  venues.push(venue)
   return {
     venues
   }
