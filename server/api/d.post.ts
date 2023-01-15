2c3ab97367276d5c249a4006a532fb2b3214f101
quizdy <quizdynet@gmail.com>
2023-01-15 11:20:09 +0900

    20230115debug8

--- server/api/d.post.ts
+++ server/api/d.post.ts
@@ -4,44 +4,38 @@ import { createCommonJS } from 'mlly'
 
 const { __dirname } = createCommonJS(import.meta.url)
 
-const ROOT_DIR = '../../public/aaa/bbb'
+const ROOT_DIR = '../../public/'
 
 export default defineEventHandler(async(e) => {
 
-  const dir = path.join(__dirname, ROOT_DIR)
-
   let ret = {
     dir: false,
-    dir1: false,
-    dir2: false,
     filessss: false,
     aaa: '',
     msg: ""
   };
+  
+  if(fs.existsSync(path.join(__dirname, '../../public/'))) {ret.dir =true}
+
+  const dir = path.join(__dirname, ROOT_DIR)
 
   if (!fs.existsSync(dir)) {
-
     fs.mkdirSync(dir, { recursive: true })
-    // const basePath = path.join(__dirname, ROOT_DIR)
-    // mkDirByPathSync(basePath)
   }
   else {
     ret.dir = false
   }
 
-  const aaa = path.join(__dirname, ROOT_DIR, 'test.txt');
+  const aaa = path.join(__dirname, ROOT_DIR, 'a.txt');
 
   try {
-    fs.writeFileSync(aaa, 'test_test_aaaaaaaaaaaaaaa')
+    fs.writeFileSync(aaa, 'adfdfafdfdad')
     ret.msg = 'write ok'
   }
   catch(e) {
     ret.msg = JSON.stringify(e)
   }
   
-  if(fs.existsSync(path.join(__dirname, '../../public/'))) {ret.dir =true}
-  if(fs.existsSync(path.join(__dirname, '../../public/aaa'))) {ret.dir1 =true}
-  if(fs.existsSync(path.join(__dirname, '../../public/aaa/bbb'))) {ret.dir2 =true}
   if(fs.existsSync(path.join(__dirname, '../../public/aaa/bbb/test.txt'))) {ret.filessss =true}
   ret.aaa = aaa
 
@@ -49,32 +43,3 @@ export default defineEventHandler(async(e) => {
     ret: ret
   }
 })
-
-function mkDirByPathSync(targetDir, { isRelativeToScript = false } = {}) {
-  const sep = path.sep;
-  const initDir = path.isAbsolute(targetDir) ? sep : '';
-  const baseDir = isRelativeToScript ? __dirname : '.';
-
-  return targetDir.split(sep).reduce((parentDir, childDir) => {
-    const curDir = path.resolve(baseDir, parentDir, childDir);
-    try {
-      fs.mkdirSync(curDir);
-    } catch (err) {
-      if (err.code === 'EEXIST') { // curDir already exists!
-        return curDir;
-      }
-
-      // To avoid `EISDIR` error on Mac and `EACCES`-->`ENOENT` and `EPERM` on Windows.
-      if (err.code === 'ENOENT') { // Throw the original parentDir error on curDir `ENOENT` failure.
-        throw new Error(`EACCES: permission denied, mkdir '${parentDir}'`);
-      }
-
-      const caughtErr = ['EACCES', 'EPERM', 'EISDIR'].indexOf(err.code) > -1;
-      if (!caughtErr || caughtErr && curDir === path.resolve(targetDir)) {
-        throw err; // Throw if it's just the last created dir.
-      }
-    }
-
-    return curDir;
-  }, initDir);
-}
